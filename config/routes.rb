8e28d321a73892a6591f32b81212565883f0e60f
atsede selassie <atsede@gmail.com>
2020-01-12 18:49:56 +0000

    first rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get 'restaurants', to: 'restaurants#index'
+
+  post 'restaurants', to: 'restaurants#create'
+
+  root to: 'pages#home'
+  # VERB PATH, to: 'CONTROLLER#ACTION'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
